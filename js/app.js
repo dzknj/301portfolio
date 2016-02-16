43885afdc23608e77fcef601e3871a701136a684
David Michael Zalk <dzknj@outlook.com>
2016-02-15 20:56:04 -0800

    linting complete

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 
 (function(module){
 
-projects = [];
+  projects = [];
 
-function ProjectItem(obj) {
-  this.title = obj.title;
-  this.author = obj.author;
-  this.projectUrl = obj.projectUrl;
-  this.description = obj.description;
-  this.publishedOn = obj.publishedOn;
-  this.boot = obj.boot;
-};
+  function ProjectItem(obj) {
+    this.title = obj.title;
+    this.author = obj.author;
+    this.projectUrl = obj.projectUrl;
+    this.description = obj.description;
+    this.publishedOn = obj.publishedOn;
+    this.boot = obj.boot;
+  };
 
 /*
 var el = new ProjectItem('busmall','david zalk', 'http://www.google.com', 'Test for placing projects in my webpage', '12/12/2012');
 var el2 = new ProjectItem('busmall','james', 'http://www.google.com', 'Test for placing projects in my webpage', '12/12/2012');
 */
 
-ProjectItem.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#article-template').text());
+  ProjectItem.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#article-template').text());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.monthsAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/30/1000);
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.monthsAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/30/1000);
 
-  this.complete = this.daysAgo + ' days ago / ' + this.monthsAgo + ' months ago';
+    this.complete = this.daysAgo + ' days ago / ' + this.monthsAgo + ' months ago';
 
     return template(this);
 //   var $newProjectList = $('article.template').clone();
@@ -42,45 +42,45 @@ ProjectItem.prototype.toHtml = function() {
 //
 //   $newProjectList.removeClass('template');
 //   return $newProjectList;
-};
+  };
 
-ProjectItem.loadAll = function(data) {
-
-
-  projects = data.map(function(stuff) {
-    console.log(data);
-    console.log(stuff);
-    return new ProjectItem(stuff);
-  })
-  var boot = data.map(function(a){
-    return parseInt(a.boot);
-  })
-  var bootTotal = boot.reduce(function(a,b){
-    return a + b;
-  });
-  console.log(boot);
-  console.log(bootTotal);
-  $('.footer').append(' There are ' + bootTotal + ' boots total in this biznatch!! ');
-  // data.forEach(function(ele) {
-  //   projects.push(new ProjectItem(ele));
-  // });
+  ProjectItem.loadAll = function(data) {
 
 
-};
-ProjectItem.fetchAllFromServer = function(callback) {
-  console.log('fetching data from server');
-  $.ajax({
-    type: 'GET',
-    url: 'js/data.json',
-    success: function(data, message, xhr) {
+    projects = data.map(function(stuff) {
+      console.log(data);
+      console.log(stuff);
+      return new ProjectItem(stuff);
+    });
+    var boot = data.map(function(a){
+      return parseInt(a.boot);
+    });
+    var bootTotal = boot.reduce(function(a,b){
+      return a + b;
+    });
+    console.log(boot);
+    console.log(bootTotal);
+    $('.footer').append(' There are ' + bootTotal + ' boots total in this biznatch!! ');
+    // data.forEach(function(ele) {
+    //   projects.push(new ProjectItem(ele));
+    // });
 
-      localStorage.eTag = xhr.getResponseHeader('eTag');
-      ProjectItem.data = data;
-      localStorage.data = JSON.stringify(data);
-      ProjectItem.loadAll(ProjectItem.data);
-      callback();
-    }
-  });
-};
-module.ProjectItem = ProjectItem;
+
+  };
+  ProjectItem.fetchAllFromServer = function(callback) {
+    console.log('fetching data from server');
+    $.ajax({
+      type: 'GET',
+      url: 'js/data.json',
+      success: function(data, message, xhr) {
+
+        localStorage.eTag = xhr.getResponseHeader('eTag');
+        ProjectItem.data = data;
+        localStorage.data = JSON.stringify(data);
+        ProjectItem.loadAll(ProjectItem.data);
+        callback();
+      }
+    });
+  };
+  module.ProjectItem = ProjectItem;
 })(window);
--- js/data.js
+++ js/data.js
@@ -1,27 +1,27 @@
 var data = [
   {
-  title: 'busmall',
-  author: 'david zalk',
-  projectUrl: 'http://www.google.com',
-  description: '<p>description description description</p><p>Bacon ipsum dolor amet do <img src="calvinpanel.jpg" class="pullright"> ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p>',
-  publishedOn: '12/12/2012'
-},
+    title: 'busmall',
+    author: 'david zalk',
+    projectUrl: 'http://www.google.com',
+    description: '<p>description description description</p><p>Bacon ipsum dolor amet do <img src="calvinpanel.jpg" class="pullright"> ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p>',
+    publishedOn: '12/12/2012'
+  },
 
 
-{
-title: 'jersey',
-author: 'spaceman',
-projectUrl: 'http://www.google.com',
-description: '<p>description description description</p><p><img src="calvinpanel2.jpg" class="pullleft"> Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p>',
-publishedOn: '9/13/2007'
-},
+  {
+    title: 'jersey',
+    author: 'spaceman',
+    projectUrl: 'http://www.google.com',
+    description: '<p>description description description</p><p><img src="calvinpanel2.jpg" class="pullleft"> Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p>',
+    publishedOn: '9/13/2007'
+  },
 
 
-{
-  title: 'haggerton',
-  author: 'james',
-  projectUrl: 'http://www.google.com',
-  description: '<p>description description description</p><p>Bacon ipsum dolor amet do<img src="calvinpanel3.jpg" class="pullright"> ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p>',
-  publishedOn: '11/11/2011'
-}
+  {
+    title: 'haggerton',
+    author: 'james',
+    projectUrl: 'http://www.google.com',
+    description: '<p>description description description</p><p>Bacon ipsum dolor amet do<img src="calvinpanel3.jpg" class="pullright"> ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p>',
+    publishedOn: '11/11/2011'
+  }
 ];
